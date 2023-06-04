fe91020fcf882ca40793f11dcc748b23edd4eade
Maxim Velgach <lordgprs@yandex.ru>
2023-06-04 10:46:44 +0300

    Some fixes

--- 06_07_01_ELECTRONICS/main.cpp
+++ 06_07_01_ELECTRONICS/main.cpp
@@ -11,12 +11,12 @@
 int main() {
 	const int kGoodCount = 5;
 
-	IElectronics *electronics[kGoodCount];
-	electronics[0] = new Player("Sony", "Walkman NW-A105B", 72, 100);
-	electronics[1] = new Laptop("Honor", "MagicBook 16 R5/16/512", 4, 1800,  15.6, 16384, 512);
-	electronics[2] = new WashingMachine("Haier", "HW60-BP10959A", 64000, 8, 1200);
-	electronics[3] = new Smartphone("Samsung", "Galaxy S22", 36, kAndroid, 8, 256);
-	electronics[4] = new Hob("Haier", "HHY-Y53NVB", 7800, kInduction, 3, 8000);
+	IElectronics *electronics[kGoodCount]; // ћассив указателей на абстрактный базовый класс
+	electronics[0] = new Player("Sony", "Walkman NW-A105B", 72, 100); // Ёкземпл€р дочернего класса портативной электроники
+	electronics[1] = new Laptop("Honor", "MagicBook 16 R5/16/512", 4, 1800,  15.6, 16384, 512);	// ѕример ромбовидного наследовани€
+	electronics[2] = new WashingMachine("Haier", "HW60-BP10959A", 64000, 8, 1200); // Ёкземпл€р дочернего класса бытовой электроники
+	electronics[3] = new Smartphone("Samsung", "Galaxy S22", 36, kAndroid, 8, 256); // Ёкземпл€р дочернего класса портативной электроники
+	electronics[4] = new Hob("Haier", "HHY-Y53NVB", 7800, kInduction, 3, 8000); // Ёкземпл€р дочернего класса бытовой электроники
 
 	setlocale(LC_ALL, "");
 	bool loop = true;
